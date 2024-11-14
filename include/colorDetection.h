860f0530d4286c2389cea1b6c1ca8888a4f14580
raphstonge <raphaelstonge@icloud.com>
2024-11-14 11:21:52 -0500

    added logique exec

--- include/colorDetection.h
+++ include/colorDetection.h
@@ -6,8 +6,8 @@
 #define COLORDETECTION_H
 #include "Adafruit_TCS34725.h"
 #include <Arduino.h>
-//#include "Adafruit_I2CDevice.h"
-//#include "Adafruit_BusIO_Register.h"
+#include "Adafruit_I2CDevice.h"
+#include "Adafruit_BusIO_Register.h"
 #include <LibRobus.h>
 void initColorSensor(Adafruit_TCS34725 tcs);
 bool isBlue (Adafruit_TCS34725 tcs);
